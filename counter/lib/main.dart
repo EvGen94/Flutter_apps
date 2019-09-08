7bec64b0670a30c137522681cff8381a3844b745
EvGen94 <sharanov.evgeniigoogl@gmail.com>
2019-09-08 22:08:25 +0300

    add_2

--- counter/lib/main.dart
+++ counter/lib/main.dart
@@ -21,28 +21,28 @@ class _MyAppState extends State<MyApp> {
     {
       'questionText': 'What\'s your favorite color?',
       'answers': [
-        {'text': 'Black', 'score': 10},
-        {'text': 'Red', 'score': 5},
-        {'text': 'Green', 'score': 3},
-        {'text': 'White', 'score': 1},
+        {'text': 'Black', 'score': 15},
+        {'text': 'Red', 'score': 8},
+        {'text': 'Green', 'score': 5},
+        {'text': 'White', 'score': 2},
       ],
     },
     {
       'questionText': 'What\'s your favorite animal?',
       'answers': [
-        {'text': 'Rabbit', 'score': 3},
-        {'text': 'Snake', 'score': 11},
-        {'text': 'Elephant', 'score': 5},
-        {'text': 'Lion', 'score': 9},
+        {'text': 'Rabbit', 'score': 4},
+        {'text': 'Snake', 'score': 12},
+        {'text': 'Elephant', 'score': 4},
+        {'text': 'Lion', 'score': 10},
       ],
     },
     {
       'questionText': 'Who\'s your favorite instructor?',
       'answers': [
-        {'text': 'Max', 'score': 1},
-        {'text': 'Max', 'score': 1},
-        {'text': 'Max', 'score': 1},
-        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 3},
+        {'text': 'Max', 'score': 3},
+        {'text': 'Max', 'score': 3},
+        {'text': 'Max', 'score': 3},
       ],
     },
   ];
